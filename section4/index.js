1e5e60e7c09832e3abfd6c604bfe3fa7ca11b916
manqn <hamorimark@hotmail.com>
2022-04-09 12:54:59 +0200

    BreakItDown

---
+++ section4/index.js
@@ -0,0 +1,16 @@
+// Break it down
+
+function charCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string;
+  // values should be the counts for those characters
+}
+
+function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character...
+  // if the char is a number/letter AND key in object, add one count
+  // if the char is a number/letter AND not in object, add it and set value to 1
+  // if character is something else (space, period, etc.) dont't do anything
+  // return object at end
+}
