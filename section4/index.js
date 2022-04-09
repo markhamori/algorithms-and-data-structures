4f6ed8a8bdd593d64d9d9c7abea8c51dd1196f13
manqn <hamorimark@hotmail.com>
2022-04-09 20:15:56 +0200

    Divide-and-Conquer-Pattern

--- section4/index.js
+++ section4/index.js
@@ -339,3 +339,36 @@ function maxSubarraySumRef(arr, num) {
 // maxSubarraySumRef([4, 2, 1, 6], 1); // 6
 // maxSubarraySumRef([4, 2, 1, 6, 2], 4); // 13
 // maxSubarraySumRef([], 4); // null
+
+// Divide and conquer pattern
+
+// NAIVE SOLUTION - linear search (Time complexity O(n))
+function search(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//Refactored Solution - binary search (Time complexity log(n))
+function searchRef(array, val) {
+  let min = 0;
+  let max = array.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = array[middle];
+
+    if (array[middle] < val) {
+      min = middle + 1;
+    } else if (array[middle] > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+
+  return -1;
+}
