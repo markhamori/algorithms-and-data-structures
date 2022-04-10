8688c0b7ae9207c5c16852a7285deb23bb907719
manqn <hamorimark@hotmail.com>
2022-04-10 13:42:31 +0200

    Section6- Challenges

--- section6/index.js
+++ section6/index.js
@@ -59,6 +59,56 @@ function areThereDuplicates2() {
   return new Set(arguments).size !== arguments.length;
 }
 
-console.log(areThereDuplicates2(1, 2, 3)); // false
-console.log(areThereDuplicates2(1, 2, 2)); // true
-console.log(areThereDuplicates2("a", "b", "c", "a")); //true
+// console.log(areThereDuplicates2(1, 2, 3)); // false
+// console.log(areThereDuplicates2(1, 2, 2)); // true
+// console.log(areThereDuplicates2("a", "b", "c", "a")); //true
+
+// Multiple Pointers - Time: O(n), Space O(1)
+function averagePair(arr, num) {
+  //Edge case(s)
+  if (arr.length === 0) return false;
+
+  // Init
+  let i = 0;
+  let j = arr.length - 1;
+
+  // Loop through the array
+  while (i < j) {
+    let avg = arr[i] + arr[j] / 2;
+
+    if (avg === num) {
+      return true;
+    } else if (avg > num) {
+      j--;
+    } else {
+      i++;
+    }
+  }
+  return false;
+}
+
+// console.log(averagePair([1, 2, 3], 2.5)); //true
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); //true
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); //false
+// console.log(averagePair([], 4)); //false
+
+// Multiple Pointers - Time: O(n+m), Space: O(1)
+function isSubsequence(str1, str2) {
+  // Edge case(s)
+
+  // Init
+  let i = 0;
+  let j = 0;
+
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world")); // true
+console.log(isSubsequence("sing", "string")); // true
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false (order matters)
