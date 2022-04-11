dfa4bc382b83212ac28ddbb2c203a18ef0207779
manqn <hamorimark@hotmail.com>
2022-04-11 20:10:11 +0200

    Section 10 - Naive string search

--- section10/index.js
+++ section10/index.js
@@ -22,8 +22,33 @@ function binarySearch(arr, val) {
 
     middle = Math.floor((start + end) / 2);
   }
-
   return arr[middle] === val ? middle : -1;
 }
 
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 9));
+// console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 3));
+
+// [2, 5, 6, 9, 13, 15, 28, 30]
+//  S        M              E
+
+// [2, 5, 6, 9, 13, 15, 28, 30]
+//              S   M       E
+
+// [2, 5, 6, 9, 13, 15, 28, 30]
+//                      SM  E
+
+// [2, 5, 6, 9, 13, 15, 28, 30]
+//                          SME
+
+// Naive string search implementation
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveSearch("lorie loled", "lol");
