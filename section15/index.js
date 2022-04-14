21fb54490c8561900398a1bdcfda50d03a8af447
manqn <hamorimark@hotmail.com>
2022-04-14 20:15:19 +0200

    Merge sort

--- section15/index.js
+++ section15/index.js
@@ -33,4 +33,12 @@ function merge(arr1, arr2) {
   return results;
 }
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([10, 24, 76, 73]));
