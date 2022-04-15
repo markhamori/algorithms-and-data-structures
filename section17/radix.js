3cb2d1027b01fe42b87f65a00748665cac69e955
manqn <hamorimark@hotmail.com>
2022-04-15 13:28:20 +0200

    Radix sort algo

---
+++ section17/radix.js
@@ -0,0 +1,34 @@
+// Radix sort helpers
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []); // Create 10 empty arrays
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      // It gives back every numbers first el (from the back) value
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
