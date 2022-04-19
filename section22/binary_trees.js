20c351def45d69a741ffbf0bb8958854c6465a27
manqn <hamorimark@hotmail.com>
2022-04-19 19:21:19 +0200

    Binary Search Tree

---
+++ section22/binary_trees.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+          } else {
+            current = current.right;
+            return this;
+          }
+        }
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root;
+    var found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+//      10
+//  5       13
+//2   7  11   16
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.insert(5);
+tree.insert(2);
+console.log(tree.insert(13));
