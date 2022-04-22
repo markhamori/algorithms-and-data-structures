3381c845b539d153c4d92a63f8b0d3c06963a803
manqn <hamorimark@hotmail.com>
2022-04-22 18:34:36 +0200

    Hash tables

---
+++ section25/hash_tables.js
@@ -0,0 +1,13 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  for (let char of key) {
+    // map "a" to 1, "b" to 2, "c" to 3, etc.
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLen;
+  }
+  return total;
+}
+
+console.log(hash("pink", 10)); // 0
+console.log(hash("orangered", 10)); // 7
+console.log(hash("cyan", 10)); // 3
