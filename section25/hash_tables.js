bd308438eee9b524f389c0f4fd68b8b5f8d10bf3
manqn <hamorimark@hotmail.com>
2022-04-22 19:44:57 +0200

    Hash table

--- section25/hash_tables.js
+++ section25/hash_tables.js
@@ -1,13 +1,26 @@
-function hash(key, arrayLen) {
-  let total = 0;
-  for (let char of key) {
-    // map "a" to 1, "b" to 2, "c" to 3, etc.
-    let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
 }
 
-console.log(hash("pink", 10)); // 0
-console.log(hash("orangered", 10)); // 7
-console.log(hash("cyan", 10)); // 3
+let ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
