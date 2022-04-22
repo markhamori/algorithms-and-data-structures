b3e7a97aee6f642c385ebf2623ebbd8d9d27d242
manqn <hamorimark@hotmail.com>
2022-04-22 20:10:42 +0200

    Hash table

--- section25/hash_tables.js
+++ section25/hash_tables.js
@@ -20,7 +20,53 @@ class HashTable {
     }
     this.keyMap[index].push([key, value]);
   }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1]))
+            valuesArr.push(this.keyMap[i][j][1]);
+        }
+      }
+      return valuesArr;
+    }
+  }
+
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0]))
+            keysArr.push(this.keyMap[i][j][0]);
+        }
+      }
+      return keysArr;
+    }
+  }
 }
 
-let ht = new HashTable();
-console.log(ht.set("hello world", "goodbye!!"));
+let ht = new HashTable(17);
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+ht.set("purple", "#DDA0DD");
+ht.set("violet", "#DDA0DD");
+console.log(ht.keys());
