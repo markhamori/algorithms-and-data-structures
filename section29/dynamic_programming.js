a5e845bbfdfa43e46a90721472c5f2bfecafb13b
manqn <hamorimark@hotmail.com>
2022-04-24 14:21:58 +0200

    Dynamin Programming

---
+++ section29/dynamic_programming.js
@@ -0,0 +1,29 @@
+// Unoptimized recursuion fib
+
+// function fib(n) {
+//   if (n <= 2) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// Memoized solution
+
+// function fib(n, memo = []) {
+//   if (memo[n] !== undefined) return memo[n];
+//   if (n <= 2) return 1;
+//   var res = fib(n - 1, memo) + fib(n - 2, memo);
+//   memo[n] = res;
+//   return res;
+// }
+
+// Tabulated version (from bottom-up!)
+
+function fib(n) {
+  if (n <= 2) return 1;
+  var fibNums = [0, 1, 1];
+  for (var i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
+
+console.log(fib(5));
