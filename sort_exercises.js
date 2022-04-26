f65116268908e5e27b1cbc72628987d72429b8cc
Hámori Márk <hamorimark@hotmail.com>
2022-04-26 14:51:08 +0200

    Exercises

--- sort_exercises.js
+++ sort_exercises.js
@@ -75,3 +75,18 @@ console.log(selectionSort([5, 3, 4, 1, 2]));
 //
 // lowest
 //  [5,   3, 4,   1,   2]
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+}
